c53db970454b981544f8da1e6b38ea13e4f78b58
anshu <anshu@192.168.0.19>
2023-01-15 15:40:49 -0500

    commit 2

--- src/test/java/comApiHackathon/stepDefinitions/batch/UpdateBatchStepDef.java
+++ src/test/java/comApiHackathon/stepDefinitions/batch/UpdateBatchStepDef.java
@@ -1,11 +1,15 @@
 package comApiHackathon.stepDefinitions.batch;
 
 import io.restassured.RestAssured;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured.*;
 import io.restassured.builder.RequestSpecBuilder;
+import static org.hamcrest.CoreMatchers.is;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import io.cucumber.java.en.*;
 import io.restassured.http.ContentType;
@@ -20,13 +24,13 @@ RequestSpecification req;
 RequestSpecification res;
 Response response;
 Response response1;
-static String bId = "788";
+ String bId = "788";
 String bname = "MathsClass";
 String bdesc = "classes for maths";
 String bstatus = "Active";
 String bNoOfClasses = "21";
-static String progId = "458";
-static String progName = "Jan23-InterfaceStrikers-SDET-100";
+ String progId = "458";
+ String progName = "Jan23-InterfaceStrikers-SDET-100";
 
 @Given("A Service URL with URI path")
 public void a_service_url_with()
@@ -39,8 +43,8 @@ res = RestAssured.given().spec(req).log().all();
 @When ("Put request is made as parameters at Path {string}, {string}, {string}, {string}, {string},{string},{string} are modified")
 public void are_modified(String batchId, String batchName, String batchDescription, String batchStatus, String batchNoOfClasses, String programId, String programName)
 {
-response = (Response) res.body("{\"batchId\":\" "+batchId+"\",\"batchName\":\""+batchName+"\",\"batchDescription\": \" " +batchDescription+"\",\"batchStatus\": \" " +batchStatus+"\", \"batchNoOfClasses\":\" "+batchNoOfClasses+"\", \"programId\":\" "+programId+"\", \"programName\":\" "+programName+"\"}").when().get("/batches/788");
-response1 = (Response) res.body("{\"batchId\":\"     "+bId+"\",\"batchName\":\""+ bname + "\", \"batchDescription\": \"" + bdesc + "\",        \"batchStatus\": \" " +bstatus+     "\",\"batchNoOfClasses\": \" " +bNoOfClasses+"\", \"programId\": \" " +progId+ "\",           \"programName\":\" "          +progName+ "\"}").when().put("/batches/788");
+response = (Response) res.body ("{\"batchId\":\" "+batchId+"\",\"batchName\":\""+batchName+"\",\"batchDescription\": \" " +batchDescription+"\",\"batchStatus\": \"" +batchStatus+"\", \"batchNoOfClasses\":\" "+batchNoOfClasses+"\", \"programId\":\" "+programId+"\", \"programName\":\" "+programName+"\"}").when().get("/batches/788");
+response1 = (Response) res.body("{\"batchId\":\"     "+bId+"\",\"batchName\":\""+ bname + "\", \"batchDescription\": \"" + bdesc + "\",         \"batchStatus\": \"" +bstatus+     "\",\"batchNoOfClasses\": \" " +bNoOfClasses+"\", \"programId\": \" " +progId+ "\",           \"programName\":\" "          +progName+ "\"}").when().put("/batches/788");
 }
 
 @Then("Validate response status code")
@@ -52,7 +56,32 @@ public void validate_response_status_code()
      {
     	 System.out.println("Your update is successful");
      }
+ }
 
-    	 
+@Then("Validate {string}, {string}, {string}, {string}, {string},{string},{string}")
+public void validate(String batchId, String batchName, String batchDescription, String batchStatus, String batchNoOfClasses, String programId, String programName)
+{
+	
+	System.out.println("The batch name in my response is "+ response1.asString());
+ response1.then().
+ assertThat()
+ 				//.body(("batchId") ,Matchers.is(bId))
+		     .body(("batchName") ,Matchers.equalTo(bname))
+		     .body("batchDescription" ,Matchers.equalTo(bdesc))
+		     .body("batchStatus" ,Matchers.equalTo(bstatus))
+		     //.body("batchNoOfClasses" ,Matchers.equalTo(bNoOfClasses))
+		     //.body("programId" ,Matchers.equalTo(progId))
+		     .body("programName" ,Matchers.equalTo(progName)).log().all();
+ 
+//	response1.then().body("batchId", equals(bId));
+//	response1.then().body("batchName", equalTo(bname));
+//	res.then().body("batchDescription", equalTo(bdesc));
+//	res.then().body("batchStatus", equalTo(bstatus));
+//	res.then().body("batchNoOfClasses", equalTo(bNoOfClasses));
+//	res.then().body("programId", equalTo(progId));
+//	res.then().body("programName", equalTo(progName));
+
 }
+
 }
+ 
--- src/test/resources/features/BatchModule/UpdateBatchById.feature
+++ src/test/resources/features/BatchModule/UpdateBatchById.feature
@@ -1,9 +1,12 @@
-Feature: Program
+Feature: Update Batch
 Scenario Outline: Update Batch
 Given A Service URL with URI path
 When Put request is made as parameters at Path "<batchId>", "<batchName>", "<batchDescription>", "<batchStatus>", "<batchNoOfClasses>","<programId>","<programName>" are modified
 Then Validate response status code
+And Validate "batchId", "batchName", "batchDescription", "batchStatus", "batchNoOfClasses","programId","programName"
 
 Examples:
   | batchId     | batchName   |batchDescription  |batchStatus | batchNoOfClasses     |  programId  | programName|
   | 788         |Interface strikeroo 06 | Automation       | Active     | 12 | 458 |Jan23-InterfaceStrikers-SDET-100|
+
+
