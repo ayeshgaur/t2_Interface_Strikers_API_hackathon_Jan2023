3c0dde83c2e99649fa606ec99574bb87552c7888
janga <janga@DESKTOP-RGL4EEL.mynetworksettings.com>
2023-01-16 15:42:56 -0500

    first commit

---
+++ src/test/java/com/api/hackathon/steps/program/DeleteProgByPrognameStepDef.java
@@ -0,0 +1,53 @@
+package com.api.hackathon.steps.program;
+
+import static io.restassured.RestAssured.given;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class DeleteProgByPrognameStepDef {
+	
+	RequestSpecification req;
+	RequestSpecification res;
+	 Response response;
+	 
+	@Given("A Service with URL to Delete Program By Name")
+	public void a_service_with_url_to_delete_program_by_name() {
+		req =new RequestSpecBuilder().setBaseUri("https://lms-backend-service.herokuapp.com/lms/")
+				.setContentType(ContentType.JSON).build();
+		  res=given().spec(req); 
+		
+	}
+
+	@When("DELETE request is made for Program by passing Program Name as a parameter")
+	public void delete_request_is_made_for_program_by_passing_program_name_as_a_parameter() {
+	     response=res.when().delete("/deletebyprogname/API Automation").then().extract().response();
+
+	}
+
+	@Then("the API call is successful for program deletion with status code {int}")
+	public void the_api_call_is_successful_for_program_deletion_with_status_code(Integer int1) {
+		
+		Assert.assertEquals(response.getStatusCode(), 200);
+
+	}
+
+	@Then("the API call results in a bad status code")
+	public void the_api_call_results_in_a_bad_status_code() {
+	    
+		response.then().log().all().assertThat().statusCode(404).body("errorCode",
+				Matchers.equalTo("Not Found"));
+	}
+
+
+}
+
+}
---
+++ src/test/resources/features/programModule/deleteProgramByProgname.feature
@@ -0,0 +1,13 @@
+Feature: Deletion in Program module By Name
+
+@tag1
+Scenario: Delete Program by Name
+Given A Service with URL to Delete Program By Name
+When DELETE request is made for Program by passing Program Name as a parameter
+Then the API call is successful for program deletion with status code 200
+
+@tag2
+Scenario: Delete Program by incorrect Name
+Given A Service with URL to Delete Program By Name
+When DELETE request is made for Program by passing Program Name as a parameter
+Then the API call results in a bad status code 
